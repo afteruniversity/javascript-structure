7a1b9384815fd72f2ea57cb0677e8c47f756cab9
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2025-05-23 09:03:58 -0300

    chore: array.js update

--- js-structure/array.js
+++ js-structure/array.js
@@ -2,7 +2,7 @@
 const music = ['RHCP - Dark Necessities', 'T-ARA - Bo Peep Bo Peep', 'PARAMORE - Hard Times'];
 console.log(music[0]); // Imprime 'RHCP - Dark Necessities' que está na posição 0 do index.
 
-music.push('GLICHERY - SEA OF PROBLEMS'); // inserindo uma nova fruta ao array.
+music.push('GLICHERY - SEA OF PROBLEMS'); // inserindo uma novo item ao array.
 music.pop(); // Remove o último elemento.
 
 console.log(music);
